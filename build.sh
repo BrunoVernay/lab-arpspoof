f5d0020d7f57c1f6105078e284215526f476a1d6
BrunoVernay <BrunoVern.a@gmail.com>
2018-02-09 10:51:57 +0100

    Changed names and added comments

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
 
-sudo docker build --tag=arpspoof --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy arpspoof
-sudo docker build --tag=tcpdump  --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy tcpdump
+sudo docker build --tag=arpspoofer --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy arpspoofer
+sudo docker build --tag=tcpdumper  --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy tcpdumper
